487ccecc7ff1dd90431b8f4aad541d18673738f3
Ilnar <ilnaresanov2002@gmail.com>
2023-02-15 22:41:37 +0500

    3-8

---
+++ script.js
@@ -0,0 +1,10 @@
+let x = +prompt("Enter the first number: ");
+let y = +prompt("Enter the second number: ");
+let z = +prompt("Enter the third number: ");
+let arr = [x, y, z];
+let res = (x + y + z) / arr.length;
+alert("Your result is " + res);
+console.log(x);
+console.log(y);
+console.log(z);
+console.log("Your result is " + res);
